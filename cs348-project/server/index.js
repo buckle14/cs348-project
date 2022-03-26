8c9e9202a325be8cdc37ade760d7240782b421ce
Vikram Raju <raju5@purdue.edu>
2022-03-26 09:36:07 -0400

    add editing functionality for coaches table

--- cs348-project/server/index.js
+++ cs348-project/server/index.js
@@ -152,7 +152,31 @@ app.post("/api/edit/games", (req, res) => {
 
 // EDIT row in the 'head_coaches' table underneath PK 'coach_id'
 app.post("/api/edit/head_coaches", (req, res) => {
-    
+    const table_name = req.body.table_name;
+    const coach_id = req.body.coach_id;
+    const team_name = req.body.team_name;
+    const coach_name = req.body.coach_name;
+    const coach_games = req.body.coach_games;
+    const coach_wins = req.body.coach_wins;
+    const coach_losses = req.body.coach_losses;
+    const coach_ties = req.body.coach_ties;
+    const coach_sb_wins = req.body.coach_sb_wins;
+    const coach_playoff_appearances = req.body.coach_playoff_appearances;
+
+    const sql = "UPDATE " + table_name + " SET" +
+                    " team_name = '" + team_name +
+                    "', coach_name = '" + coach_name +
+                    "', coach_games = '" + coach_games +
+                    "', coach_wins = '" + coach_wins +
+                    "', coach_losses = '" + coach_losses +
+                    "', coach_ties = '" + coach_ties +
+                    "', coach_sb_wins = '" + coach_sb_wins +
+                    "', coach_playoff_appearances = '" + coach_playoff_appearances +
+                    "' WHERE coach_id = '" + coach_id + "';";
+    db.query(sql, (err, result) => {
+        console.log(err)
+        res.send(err);
+    })
 })
 
 // EDOT row in the 'offensive_players' table underneath PK 'off_id'
